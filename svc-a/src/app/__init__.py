f0f3f6d85c542d2ebb05056e436b764b4eada8a0
Elias Brange <elias.brange@gmail.com>
2022-03-03 19:47:13 +0100

    Read dynamodb table name from SSM in lambda

--- svc-a/src/app/__init__.py
+++ svc-a/src/app/__init__.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from mangum import Mangum
+import os
+import boto3
+
+ssm_client = boto3.client("ssm")
+
+table_name = ssm_client.get_parameter(
+    Name=os.environ["DYNAMO_TABLE_SSM"]
+)["Parameter"]["Value"]
 
 app = FastAPI()
 
@@ -7,7 +15,7 @@ app = FastAPI()
 @app.get("/")
 def get_root():
     print("test")
-    return {"message": "Hello World"}
+    return {"message": f"Hello World {table_name}"}
 
 
 handler = Mangum(app)
