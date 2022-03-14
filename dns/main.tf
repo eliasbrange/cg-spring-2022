51986c768703c920cfb95fab0c963386ce209a54
Elias Brange <elias.brange@gmail.com>
2022-03-14 23:02:14 +0100

    Add SSM param for Hosted Zone Id in us-east-1

--- dns/main.tf
+++ dns/main.tf
@@ -15,3 +15,8 @@ terraform {
 provider "aws" {
   region = "eu-west-1"
 }
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "useast"
+}
--- dns/route53.tf
+++ dns/route53.tf
@@ -8,6 +8,13 @@ resource "aws_ssm_parameter" "hosted_zone_id" {
   value = aws_route53_zone.zone.zone_id
 }
 
+resource "aws_ssm_parameter" "hosted_zone_id_useast" {
+  provider = aws.useast
+  name  = "/eliasb/hosted_zone_id"
+  type  = "String"
+  value = aws_route53_zone.zone.zone_id
+}
+
 resource "aws_route53_record" "root_record" {
   zone_id = aws_route53_zone.zone.zone_id
   name    = "aws.eliasbrange.dev"
