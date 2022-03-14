5213ed1970cede7d2b9695ca2194b8a94b27ddb4
Elias Brange <elias.brange@gmail.com>
2022-03-14 23:01:58 +0100

    Add CI bucket in us-east-1

---
+++ ci/buckets.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "ci_bucket" {
+  bucket = "elias-brange-ci-cd"
+}
+
+resource "aws_s3_bucket_versioning" "ci_bucket_versioning" {
+  bucket = aws_s3_bucket.ci_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "ci_bucket_useast" {
+  provider = aws.useast
+  bucket = "elias-brange-ci-cd-us-east-1"
+}
+
+resource "aws_s3_bucket_versioning" "ci_bucket_versioning-useast" {
+  provider = aws.useast
+  bucket = aws_s3_bucket.ci_bucket_useast.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
