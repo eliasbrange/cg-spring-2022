e1cd2470c4d16227b8dec7ed2230c58a72150d05
Elias Brange <elias.brange@gmail.com>
2022-03-15 08:47:17 +0100

    Update edge origin request lambda

--- edge-api/origin-request/app.py
+++ edge-api/origin-request/app.py
@@ -1,3 +1,38 @@
+from aws_lambda_powertools import Logger
+
+logger = Logger(service="payment")
+
+
+DOMAINS = [
+    {
+        "path": "/cats",
+        "domain": "cats.aws.eliasbrange.dev",
+    },
+    {
+        "path": "/dogs",
+        "domain": "dogs.aws.eliasbrange.dev",
+    },
+]
+
+
+@logger.inject_lambda_context
 def handler(event, context):
-    request = event["Records"][0]["cf"]
-    print(request)
+    request = event["Records"][0]["cf"]["request"]
+    logger.info("Request received", extra=request)
+
+    uri = request["uri"]
+
+    for d in DOMAINS:
+        if uri.startswith(d["path"]):
+            request["origin"]["custom"]["domainName"] = d["domain"]
+            host = [{"key": "host", "value": d["domain"]}]
+            request["headers"]["host"] = host
+            break
+    else:
+        return {
+            "status": 404,
+            "statusDescription": "Not Found",
+        }
+
+    logger.info("Request returned", extra=request)
+    return request
