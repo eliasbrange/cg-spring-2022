f5c6044d1f4e96f7aab45f11c9ba11ae46c89d7f
Elias Brange <elias.brange@gmail.com>
2022-04-04 15:06:15 +0200

    Add access to bucket

--- ci/ci-roles.tf
+++ ci/ci-roles.tf
@@ -18,3 +18,40 @@ resource "aws_iam_role" "auth-pr" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "auth-pr-buckets" {
+  role       = aws_iam_role.auth-pr.name
+  policy_arn = aws_iam_policy.state-buckets.arn
+}
+
+resource "aws_iam_policy" "state-buckets" {
+  name        = "eliasb-access-state-buckets"
+  description = "Access to state buckets"
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:ListBucket"
+        ],
+        "Resource" : [
+          aws_s3_bucket.ci_bucket.arn,
+          aws_s3_bucket.ci_bucket_useast.arn,
+        ]
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "s3:GetObject",
+          "s3:PutObject"
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.ci_bucket.arn}/*",
+          "${aws_s3_bucket.ci_bucket_useast.arn}/*",
+        ]
+      }
+    ]
+  })
+}
