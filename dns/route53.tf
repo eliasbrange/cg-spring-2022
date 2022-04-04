58c9d2217d00512ab7ffa83afec7460a175c4d6d
Elias Brange <elias.brange@gmail.com>
2022-04-04 17:44:53 +0200

    tmp

--- dns/route53.tf
+++ dns/route53.tf
@@ -8,6 +8,12 @@ resource "aws_ssm_parameter" "hosted_zone_id" {
   value = aws_route53_zone.zone.zone_id
 }
 
+resource "aws_ssm_parameter" "hosted_zone_id2" {
+  name  = "/eliasb/hosted_zone_id2"
+  type  = "String"
+  value = aws_route53_zone.zone.zone_id
+}
+
 resource "aws_ssm_parameter" "hosted_zone_id_useast" {
   provider = aws.useast
   name     = "/eliasb/hosted_zone_id"
