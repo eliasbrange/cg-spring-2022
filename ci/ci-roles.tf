16f1a0fc63374152b579beb21ccda0a32fde36be
Elias Brange <elias.brange@gmail.com>
2022-04-04 18:07:28 +0200

    Add db workflow

--- ci/ci-roles.tf
+++ ci/ci-roles.tf
@@ -55,6 +55,34 @@ module "auth_deploy_role" {
   ]
 }
 
+module "db_pr_role" {
+  source = "./ci-role"
+
+  name                     = "db-read-role"
+  subject_claim            = "repo:eliasbrange/cg-spring-2022:pull_request"
+  oidc_provider_arn        = aws_iam_openid_connect_provider.github.arn
+  state_buckets_policy_arn = aws_iam_policy.state_buckets_policy.arn
+  permissions = [
+    "dynamodb:Describe*",
+    "ssm:Describe*",
+    "ssm:Get*",
+    "ssm:List*",
+  ]
+}
+
+module "db_deploy_role" {
+  source = "./ci-role"
+
+  name                     = "db-deploy-role"
+  subject_claim            = "repo:eliasbrange/cg-spring-2022:ref:refs/heads/main"
+  oidc_provider_arn        = aws_iam_openid_connect_provider.github.arn
+  state_buckets_policy_arn = aws_iam_policy.state_buckets_policy.arn
+  permissions = [
+    "dynamodb:*",
+    "ssm:*",
+  ]
+}
+
 resource "aws_iam_policy" "state_buckets_policy" {
   name        = "eliasb-access-state-buckets"
   description = "Access to state buckets"
